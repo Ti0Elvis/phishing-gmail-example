4fdde354caee00ce41b6309d8ea8891a42b2c2a9
Ti0Elvis <elvis.veramijares@gmail.com>
2025-05-27 05:42:52 +0100

    Refactor ModeToggle component for improved readability and efficiency

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,31 +1,31 @@
 "use client";
-import { useTheme } from "next-themes";
-import { MoonIcon, SunIcon } from "lucide-react";
-// @components
 import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
-    <>
-      <Button
-        variant="outline"
-        size="icon"
-        onClick={() => {
-          if (theme === "light") {
-            setTheme("dark");
-          } else if (theme === "dark") {
-            setTheme("light");
-          }
-        }}>
-        {theme === "dark" && (
-          <MoonIcon className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-        )}
-        {theme === "light" && (
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        )}
-      </Button>
-    </>
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => {
+        setTheme(theme === "light" ? "dark" : "light");
+      }}>
+      {theme === "dark" ? (
+        <MoonIcon className="h-[1.2rem] w-[1.2rem] transition-all" />
+      ) : (
+        <SunIcon className="h-[1.2rem] w-[1.2rem] transition-all" />
+      )}
+    </Button>
   );
 }
